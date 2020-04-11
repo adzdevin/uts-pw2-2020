e2989e8705931df6cad00bcf7c7e9678b96f6974
Fiqri Syah Redha <wibulo27@gmail.com>
2020-04-11 20:27:31 +0700

    Selesai parsing

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,19 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/form', function () {
+    return view('form');
+});
+
+Route::get('/index', function () {
+    return view('index');
+});
+Route::get('/success', function () {
+    return view('success');
 });
